5882acf9c7f70b06b02613ae4e30cf70816fc158
けんし <kensiiwasaki@gmail.com>
2022-08-22 23:59:59 +0900

    COMMIT

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -8,8 +8,8 @@ const Header = () => {
         <div className={styles.container}>
           <p>豆まきブログ</p>
           <div className={styles.mini_container}>
-            <p>ポートフォリオ(仮)</p>
-            <p>趣味ギャラリー(仮)</p>
+            <p>ポートフォリオ</p>
+            <p>趣味ギャラリー</p>
           </div>
         </div>
       </div>
